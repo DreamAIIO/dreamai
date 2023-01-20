b044747df30ca3ffba1f1ddec34caf0410469552
HamzaFarhan <thehamza96@gmail.com>
2023-01-20 19:46:46 +0500

    req

--- dreamai/checkers.py
+++ dreamai/checkers.py
@@ -20,7 +20,7 @@ def list_or_tuple(x):
     return (is_list(x) or is_tuple(x))
 
 def is_iter(o):
-    "Test whether `o` can be used in a `for` loop"
+    "Test whether `o` can be used in a `for` loop."
     #Rank 0 tensors in PyTorch are not really iterable
     return isinstance(o, (Iterable,Generator)) and getattr(o,'ndim',1)
 
